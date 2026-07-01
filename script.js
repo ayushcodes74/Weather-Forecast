920c2c6a64a03dc6eb01a543f6002b783c51d85d
Ayush Raj <rajpiyush091@gmail.com>
2026-07-01 19:54:55 +0530

    Final Commit

--- script.js
+++ script.js
@@ -7,19 +7,32 @@ const cityname = document.getElementById('cityname');
 const humidity = document.getElementById('humidity');
 const windspeed = document.getElementById('windspeed');
 
- searchbar.addEventListener('submit', async (event) => {
-     event.preventDefault();
-     const city = cityInput.value.trim();
-     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-     const response = await fetch(url);
-     const data = await response.json();
-     temperature.textContent = `${data.main.temp} °C`;
-     cityname.textContent = data.name;
-     humidity.textContent = `Humidity: ${data.main.humidity}%`;
-     windspeed.textContent = `Wind Speed: ${data.wind.speed} m/s`;
-     description.textContent = data.weather[0].description;
-     weathericon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-    
-    
- });
-const API_KEY = "92d48471a8fbd0c2129343bcd240b491";
+searchbar.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const city = cityInput.value.trim();
+    if (city === "") {
+        alert("Please enter a city name.");
+        return;
+    }
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("City not found.");
+        }
+        const data = await response.json();
+        temperature.textContent = `${data.main.temp} °C`;
+        cityname.textContent = data.name;
+        humidity.textContent = `Humidity: ${data.main.humidity}%`;
+        windspeed.textContent = `Wind Speed: ${data.wind.speed} m/s`;
+        description.textContent = data.weather[0].description;
+        weathericon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+
+
+    }
+    catch (error) {
+
+        alert(error.message);}
+
+    });
+    const API_KEY = "92d48471a8fbd0c2129343bcd240b491";
